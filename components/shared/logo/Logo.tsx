9f7d2e0bed7fe3f03958dbe09030c8312935e42a
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-01 12:37:06 +0600

    chore: fixed logo and favicon

--- components/shared/logo/Logo.tsx
+++ components/shared/logo/Logo.tsx
@@ -5,10 +5,13 @@ import logo from "@/assets/logo.png";
 export default function Logo() {
   return (
     <Link href="/">
-      <div className="flex items-center space-x-2 cursor-pointer">
-        <Image src={logo} alt="Logo" width={50} height={50} />
-        <span className="text-md font-bold italic">SAJIB</span>
-      </div>
+      <Image
+        src={logo}
+        alt="Logo"
+        width={50}
+        height={50}
+        className="rounded-md border"
+      />
     </Link>
   );
 }
